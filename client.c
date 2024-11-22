3f6aafe663bc07803cc6bc502bbd57294eac11ed
Daniil <156441168+Her1sgl@users.noreply.github.com>
2024-11-23 01:48:37 +0700

    Update client.c

--- client.c
+++ client.c
@@ -1,8 +1,11 @@
-                                                                                     client.c                                                                                        Modified  
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+
 
 #define PORT 3030
 
